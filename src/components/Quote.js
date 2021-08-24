700c47980aa3ac2d6677dc19c39d128fb2e504f7
Erez <erezfree29@gmail.com>
2021-08-24 15:17:52 +0100

    add router and create design

---
+++ src/components/Quote.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Quotepage = () => (
+  <header>
+    <h2>Welcome to our page!</h2>
+    <p>
+      <span className="name">
+        Without mathematics, there’s nothing you can do. Everything around you is
+        mathematics. Everything around you is numbers.
+        —     Shakuntala Devi, Indian writer and mental calculator
+      </span>
+    </p>
+  </header>
+);
+
+export default Quotepage;
